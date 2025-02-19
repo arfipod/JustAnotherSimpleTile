afeb040638e602eee4f82f14f6331985bb841425
arfipod <arfipod@gmail.com>
2025-02-19 18:20:33 +0100

    First commit adapted to tile, complication and activity

---
+++ app/src/main/java/com/arfipod/justanothersimpletile/presentation/theme/Theme.kt
@@ -0,0 +1,17 @@
+package com.arfipod.justanothersimpletile.presentation.theme
+
+import androidx.compose.runtime.Composable
+import androidx.wear.compose.material.MaterialTheme
+
+@Composable
+fun JustAnotherSimpleTileTheme(
+    content: @Composable () -> Unit
+) {
+    /**
+     * Empty theme to customize for your app.
+     * See: https://developer.android.com/jetpack/compose/designsystems/custom
+     */
+    MaterialTheme(
+        content = content
+    )
+}
